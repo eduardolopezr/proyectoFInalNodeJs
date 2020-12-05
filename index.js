fc8b73a86ada06c0cc31f4b1dd447d48af09db58
eduardolora7 <jesuse_166@hotmail.com>
2020-12-04 21:05:55 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+/*
+req= peticion del cliente
+res= respuesta del servidor
+
+*/ 
+app.get("/", (req, res, next)=>{
+    res.status(200);
+    res.send("Bienvenidos");
+});
+app.listen(3000, ()=>{
+    console.log("Server is running");
+});
